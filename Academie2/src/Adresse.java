956404645b4bfafde3b0268ba93e617464078fcc
Florent GENIET <florent.geniet@laposte.net>
2018-04-10 16:06:30 +0200

    L'uml

---
+++ Academie2/src/Adresse.java
@@ -0,0 +1,19 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("4a72f542-2377-4fce-9354-ab508159bb70")
+public class Adresse {
+    @objid ("cb9decb5-79c4-4605-9be4-57447f5ad73d")
+    public String adresse;
+
+    @objid ("03095aba-8378-41c8-8704-c20ae28b41e0")
+    protected int coordonnees;
+
+    @objid ("bb6fecca-e212-481f-be42-f6e4b0ec58bf")
+    public Adresse() {
+    }
+
+    @objid ("40d023ea-a99c-4fee-9579-43817a14ab5b")
+    public double calculerDistance(Adresse adr) {
+    }
+
+}
